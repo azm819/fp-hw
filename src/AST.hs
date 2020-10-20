26ae553dd8d4ba5ceeaf74167711ab8b86a483a9
Azamat Farkhiev <iazamat.far@gmail.com>
2020-10-20 19:33:44 +0300

    implement edsl

--- src/AST.hs
+++ src/AST.hs
@@ -1,4 +1,8 @@
+{-# Language DeriveFunctor #-}
+
 module AST where
+
+import Control.Monad.Free
 
 data Exp = Name String
          | IntLiteral Int
@@ -30,3 +34,112 @@ data Statement = Exp Exp
                deriving (Eq, Show)
 
 type Statements = [Statement]
+
+data Cmd a = EExp Exp a
+           | EIf Exp EDSL a
+           | EElse EDSL a
+           | EElif Exp EDSL a
+           | EWhile Exp EDSL a
+           | EDef String [String] EDSL a
+           | EAssn String Exp a
+           deriving Functor
+
+type EDSL = Free Cmd ()
+
+collapseElif :: EDSL -> (Statements, EDSL)
+collapseElif (Free (EElse body next)) = (edsl body, next)
+collapseElif (Free (EElif exp body next)) = ([If exp (edsl body) body'], next')
+  where (body', next') = collapseElif next
+collapseElif x = ([], x)
+
+edsl :: EDSL -> Statements
+edsl (Pure ()) = []
+edsl (Free (EExp exp next)) = Exp exp : edsl next
+edsl (Free (EIf exp body next)) = If exp (edsl body) body' : edsl next'
+  where (body', next') = collapseElif next
+edsl (Free (EWhile exp body next)) = While exp (edsl body) : edsl next
+edsl (Free (EDef name args body next)) = Def name args (edsl body) : edsl next
+edsl (Free (EAssn name exp next)) = Assn name exp : edsl next
+
+n :: String -> Exp
+n = Name
+
+i :: Int -> Exp
+i = IntLiteral
+
+s :: String -> Exp
+s = StrLiteral
+
+e :: Exp -> EDSL
+e exp = liftF (EExp exp ())
+
+if' :: Exp -> EDSL -> EDSL
+if' exp body = liftF (EIf exp body ())
+
+else' :: EDSL -> EDSL
+else' body = liftF (EElse body ())
+
+elif :: Exp -> EDSL -> EDSL
+elif exp body = liftF (EElif exp body ())
+
+while :: Exp -> EDSL -> EDSL
+while exp body = liftF (EWhile exp body ())
+
+def :: String -> [String] -> EDSL -> EDSL
+def name args body = liftF (EDef name args body ())
+
+infix 2 $=$
+infix 3 $==$, $!=$, $<$, $<=$, $>$, $>=$
+infixl 4 `or'`
+infixl 5 `and'`
+infixl 6 $+$, $-$
+infixl 7 $*$, $/$, $%$
+infixl 8 $$
+
+not' :: Exp -> Exp
+not' = Not
+
+or' :: Exp -> Exp -> Exp
+or' = Or
+
+and' :: Exp -> Exp -> Exp
+and' = And
+
+($=$) :: String -> Exp -> EDSL
+name $=$ exp = liftF (EAssn name exp ())
+
+($==$) :: Exp -> Exp -> Exp
+($==$) = Equal
+
+($!=$) :: Exp -> Exp -> Exp
+($!=$) = NotEqual
+
+($<$) :: Exp -> Exp -> Exp
+($<$) = Less
+
+($>$) :: Exp -> Exp -> Exp
+($>$) = Greater
+
+($<=$) :: Exp -> Exp -> Exp
+($<=$) = LessEqual
+
+($>=$) :: Exp -> Exp -> Exp
+($>=$) = GreaterEqual
+
+($+$) :: Exp -> Exp -> Exp
+($+$) = Add
+
+($-$) :: Exp -> Exp -> Exp
+($-$) = Sub
+
+($*$) :: Exp -> Exp -> Exp
+($*$) = Mul
+
+($/$) :: Exp -> Exp -> Exp
+($/$) = Div
+
+($%$) :: Exp -> Exp -> Exp
+($%$) = Mod
+
+($$) :: String -> [Exp] -> Exp
+($$) = Call
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,13 @@
 module Lib ( someFunc ) where
 
+import AST
 import Parser
 import Runner
 import System.Environment
 import System.IO
 
-someFunc :: IO ()
-someFunc = do
+main' :: IO ()
+main' = do
   [filename] <- getArgs
   s <- readFile filename
   case parse filename s of
@@ -14,3 +15,16 @@ someFunc = do
     Right prog -> do
       print prog
       run prog
+
+someFunc :: IO ()
+someFunc = print $ edsl $ do
+  "n" $=$ i 0
+  while (n "n" $<$ i 100) $ do
+    if' (n "n" $%$ i 15 $==$ i 0) $ do
+      e$ "print" $$ [s "FizzBuzz"]
+    elif (n "n" $%$ i 3 $==$ i 0) $ do
+      e$ "print" $$ [s "Fizz"]
+    elif (n "n" $%$ i 5 $==$ i 0) $ do
+      e$ "print" $$ [s "Buzz"]
+    else' $ do
+      e$ "print" $$ [n "n"]
